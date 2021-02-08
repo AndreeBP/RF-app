dd0fd3e4ac4f89588fc46657d956aa9a35c7a639
Andree Bonilla <andree.bonilla@tecsup.edu.pe>
2021-02-07 20:34:51 -0500

    radiadores fortaleza1

---
+++ app/src/main/java/com/radiadores/fortaleza/Fragments/FragmentMapa.java
@@ -0,0 +1,37 @@
+package com.radiadores.fortaleza.Fragments;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.radiadores.fortaleza.MainActivity;
+import com.radiadores.fortaleza.R;
+
+public class FragmentMapa extends Fragment   {
+    //initialize variable
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.mapa_fragment ,container,false);
+        return view;
+
+
+
+    }
+
+
+}
